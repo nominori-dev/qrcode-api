8057e01b3f9c5abbd9e313c9a67ac2ff3f65cc56
nominori-dev <antonshimchico37@gmail.com>
2023-06-18 01:59:21 +0200

    simplified code in checksum algorithm

--- src/main/java/com/nominori/qrcodeapi/barcode/service/impl/ChecksumServiceImpl.java
+++ src/main/java/com/nominori/qrcodeapi/barcode/service/impl/ChecksumServiceImpl.java
@@ -4,11 +4,12 @@ import com.nominori.qrcodeapi.barcode.service.ChecksumService;
 import com.nominori.qrcodeapi.core.exception.InvalidChecksumException;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 @Service
 public class ChecksumServiceImpl implements ChecksumService {
+
+    // #TODO Needs documentation for this algorithm + refactoring.
     @Override
     public String getEAN13Checksum(String data) {
         try {
@@ -20,17 +21,12 @@ public class ChecksumServiceImpl implements ChecksumService {
             int evenSum = 0;
 
             for(int i = 0; i < array.length; i++){
-                if(i % 2 == 0){
-                    oddSum += array[i];
-                }else{
-                    evenSum += array[i];
-                }
+                if(i % 2 == 0) oddSum += array[i];
+                else evenSum += array[i];
             }
 
             int result = (evenSum * 3 + oddSum) % 10;
-            if(result > 0){
-                result = 10 - result;
-            }
+            if(result > 0) result = 10 - result;
 
             return String.valueOf(result);
         }catch (Exception exception) {
