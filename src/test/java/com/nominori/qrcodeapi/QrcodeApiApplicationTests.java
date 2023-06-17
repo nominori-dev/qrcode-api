98858912072437333fda3c7ebdb78ff3ac8e724c
nominori-dev <antonshimchico37@gmail.com>
2023-06-17 23:03:10 +0200

    Initial commit

---
+++ src/test/java/com/nominori/qrcodeapi/QrcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nominori.qrcodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
